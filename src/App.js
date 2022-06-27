8985eccf417b3c1df34ba43922aac71365e5f94f
user <user@users-MacBook-Pro.local>
2022-06-27 21:59:04 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+    const [tasks, setTasks] = useState([
+      { name: "Buy Shopping", priority: "high" },
+      { name: "Clean Bathroom", priority: "low" },
+      { name: "Car's MOT", priority: "high" },
+  ])
+    const [newTask, setNewTask] = useState("");
+  
+    const taskNodes = tasks.map((task, index) => {
+      return(
+        <li key={index} className={task.isHighPriority ? "high priority" : "low priority"}>
+          <span>{ task.name }</span>
+        
+        </li>
+      )
+    })
+
+    const handleTaskInput = (event) => {
+      setNewTask(event.item.value)
+    }
+
+    const saveNewTask = (event) => {
+      event.preventDefault();
+      const copyTasks = [...tasks]
+      copyTasks.push({name: newTask, isHighPriority: "high"})
+      setTasks(copyTasks)
+      setNewTask("")
+    }
+
+    const isHighPriority = (index) => {
+      const copyTasks = [...tasks]
+      copyTasks[index].isHighPriority = "high";
+      setTasks(copyTasks)
+    }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <header class="App-link">  
+    <h1>ToDo's</h1>
+    <hr></hr>
+  </header> 
+
+    <ul>
+      {taskNodes}
+    </ul>
+
+    <form>
+      <input type="radio" id="high" name="high priority" value="high"></input>
+      <label for="high">High</label><br></br>
+      <input type="radio" id="high" name="low priority" value="low"></input>
+      <label for="low">Low</label>
+    </form>
+
+
+    <form onSubmit={saveNewTask}>
+      <label htmlFor="new-task">Add a new Task:</label>
+      <input id="new-task" type="text" onChange={handleTaskInput}/>
+      <input type="submit" value="Save Task" />
+    </form>
+    
+  </div> 
   );
 }
 
